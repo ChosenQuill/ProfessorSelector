a837c9f64d77061a3b6c2f79ea827c8528298bcc
ChosenQuill <suredroidbot@gmail.com>
2022-11-15 06:05:01 +0000

    Directory Move + Couple Updates

---
+++ src/routes/app/Course.svelte
@@ -0,0 +1,14 @@
+<script>
+    export let info;
+    let desc;
+    console.log(info.description)
+    $: {
+        const re = /(.+hours?\)) (.+) Prerequisites?:.*/g
+        const res = re.exec(info.description);
+        desc = res ? res[2] : info.description;
+    }
+</script>
+
+<h3 class="text-2xl"><span class="font-bold">{info.title}</span><span>&nbsp;(Spring 2022)</span></h3>
+
+<p class="my-2"> {desc} </p>
