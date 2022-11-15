2c8610b181eaacf6e24469a41a977a9d9818d13a
ChosenQuill <suredroidbot@gmail.com>
2022-11-15 06:05:01 +0000

    Directory Move + Couple Updates

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<section class="hero flex-grow">
+	<div class="hero-content text-center">
+		<div class="max-w-md">
+		<h1 class="text-5xl font-bold">Hello there</h1>
+		<p class="py-6">Welcome to the UTD Professor Selector. Your all in one shop to find the best professors in town.</p>
+		<a class="btn btn-primary" href="/app">Get Started</a>
+		</div>
+	</div>
+</section>
